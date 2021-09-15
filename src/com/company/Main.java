7841b784683e145d953878be7d36d5fedd31c5eb
Lauribee <laura_stroe_2001@yahoo.com>
2021-09-15 08:43:46 +0200

    can convert a single numberToLeter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        /* testttt
+        /* testtttt
         char letter = 'A';
 
         int number = letterToNumber(letter);
